6fbb717f8f0abfd3101e06b1ed9d6c3f488262c5
atul77-ui <atulsinjali77@gmail.com>
2026-06-23 08:29:08 +0545

    assignment2

--- dbconnect.php
+++ dbconnect.php
@@ -1,8 +1,8 @@
 <?php
- //database connection variables for your UOS webspace database
- $servername = "localhost";
- $username = "";
- $password = "";
- $database = ""; 
+//database connection variables for your UOS webspace database
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "cycling";
 
- ?>
+?>
